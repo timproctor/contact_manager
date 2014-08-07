15db53caff97708929e188b9e6dafa01d8584741
timproctor <timandcamiproctor@gmail.com>
2014-08-07 10:45:34 -0600

    Passed phone-number-specs

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,6 +1,5 @@
 class PhoneNumber < ActiveRecord::Base
 	validates :number, presence: true
-	validates :person_id, presence: true
-	belongs_to :person
-	belongs_to :company
+	validates :contact_id, presence: true
+	belongs_to :contact, polymorphic: true
 end
