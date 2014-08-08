7be3cd15f167b87903fcb4d9ff18f0d4ead6b4f2
timproctor <timandcamiproctor@gmail.com>
2014-08-08 13:15:58 -0600

    refactored phone_numbers to exclude show and index

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,28 +1,13 @@
 class PhoneNumbersController < ApplicationController
   before_action :set_phone_number, only: [:show, :edit, :update, :destroy]
 
-  # GET /phone_numbers
-  # GET /phone_numbers.json
-  def index
-    @phone_numbers = PhoneNumber.all
-  end
-
-  # GET /phone_numbers/1
-  # GET /phone_numbers/1.json
-  def show
-  end
-
-  # GET /phone_numbers/new
   def new
     @phone_number = PhoneNumber.new(contact_id: params[:contact_id], contact_type: params[:contact_type])
   end
 
-  # GET /phone_numbers/1/edit
   def edit
   end
 
-  # POST /phone_numbers
-  # POST /phone_numbers.json
   def create
     @phone_number = PhoneNumber.new(phone_number_params)
 
@@ -37,8 +22,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /phone_numbers/1
-  # PATCH/PUT /phone_numbers/1.json
   def update
     respond_to do |format|
       if @phone_number.update(phone_number_params)
@@ -51,8 +34,6 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number.destroy
     respond_to do |format|
@@ -62,12 +43,10 @@ class PhoneNumbersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_phone_number
       @phone_number = PhoneNumber.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def phone_number_params
       params.require(:phone_number).permit(:number, :contact_id, :contact_type)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 
   resources :email_addresses
 
-  resources :phone_numbers
+  resources :phone_numbers, :except => [:index, :show]
 
   resources :people
 
