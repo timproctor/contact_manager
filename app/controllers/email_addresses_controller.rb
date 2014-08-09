9751ae3d479d21d5103782b37566eb7288622a61
timproctor <timandcamiproctor@gmail.com>
2014-08-09 11:12:32 -0600

    Removed related files to email_address show and index, specs all green

--- app/controllers/email_addresses_controller.rb
+++ app/controllers/email_addresses_controller.rb
@@ -1,28 +1,13 @@
 class EmailAddressesController < ApplicationController
   before_action :set_email_address, only: [:show, :edit, :update, :destroy]
 
-  # GET /email_addresses
-  # GET /email_addresses.json
-  def index
-    @email_addresses = EmailAddress.all
-  end
-
-  # GET /email_addresses/1
-  # GET /email_addresses/1.json
-  def show
-  end
-
-  # GET /email_addresses/new
   def new
     @email_address = EmailAddress.new(contact_id: params[:contact_id], contact_type: params[:contact_type])
   end
 
-  # GET /email_addresses/1/edit
   def edit
   end
 
-  # POST /email_addresses
-  # POST /email_addresses.json
   def create
     @email_address = EmailAddress.new(email_address_params)
 
@@ -37,8 +22,6 @@ class EmailAddressesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /email_addresses/1
-  # PATCH/PUT /email_addresses/1.json
   def update
     respond_to do |format|
       if @email_address.update(email_address_params)
@@ -51,8 +34,6 @@ class EmailAddressesController < ApplicationController
     end
   end
 
-  # DELETE /email_addresses/1
-  # DELETE /email_addresses/1.json
   def destroy
     @email_address.destroy
     respond_to do |format|
@@ -62,12 +43,10 @@ class EmailAddressesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_email_address
       @email_address = EmailAddress.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def email_address_params
       params.require(:email_address).permit(:email, :contact_id, :contact_type)
     end
